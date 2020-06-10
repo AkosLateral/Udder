5567af7d57192f728449ca09b377d218426c1aad
Balogh Akos <akos.balogh@lateralreality.hu>
2020-06-10 15:12:00 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Balogh Ákos on 2020. 06. 10..
+//  Copyright © 2020. Balogh Ákos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
